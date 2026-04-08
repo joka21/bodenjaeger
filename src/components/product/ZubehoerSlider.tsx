86a9c28ad9da9ca4625d9af8d0099078cc1e9af2
joka21 <jo.kalenberb@web.de>
2026-04-08 10:11:03 +0200

    update

--- src/components/product/ZubehoerSlider.tsx
+++ src/components/product/ZubehoerSlider.tsx
@@ -338,33 +338,13 @@ export default function ZubehoerSlider({
                                 </h3>
                               </Link>
 
-                              {/* Features mit Checkmarks */}
-                              <div className="space-y-1 mb-3 flex-1">
-                                {/* Paketinhalt */}
-                                {product.paketinhalt && (
-                                  <div className="flex items-start gap-1.5 text-xs text-gray-600">
-                                    <svg className="w-3.5 h-3.5 text-green-600 flex-shrink-0 mt-0.5" fill="currentColor" viewBox="0 0 20 20">
-                                      <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                                    </svg>
-                                    <span>{product.paketinhalt} {product.einheit_short || 'Stück'}</span>
-                                  </div>
-                                )}
-
-                                {/* Generic Features */}
-                                <div className="flex items-start gap-1.5 text-xs text-gray-600">
-                                  <svg className="w-3.5 h-3.5 text-green-600 flex-shrink-0 mt-0.5" fill="currentColor" viewBox="0 0 20 20">
-                                    <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                                  </svg>
-                                  <span>Hochwertige Qualität</span>
-                                </div>
-
-                                <div className="flex items-start gap-1.5 text-xs text-gray-600">
-                                  <svg className="w-3.5 h-3.5 text-green-600 flex-shrink-0 mt-0.5" fill="currentColor" viewBox="0 0 20 20">
-                                    <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                                  </svg>
-                                  <span>Schnelle Lieferung</span>
-                                </div>
-                              </div>
+                              {/* Kurzbeschreibung */}
+                              {product.short_description && (
+                                <div
+                                  className="text-xs text-gray-600 mb-3 flex-1 line-clamp-3"
+                                  dangerouslySetInnerHTML={{ __html: product.short_description }}
+                                />
+                              )}
 
                               {/* Preis */}
                               <div className="text-right">
